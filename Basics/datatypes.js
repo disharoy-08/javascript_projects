db09176b076e10b1d929251119aaf3d189d8b752
disharoy-08 <disharoy0827@gmail.com>
2023-10-02 10:43:06 +0530

    learned conversion

--- Basics/datatypes.js
+++ Basics/datatypes.js
@@ -1,2 +1,43 @@
-console.log(2+2)
-console.log("Geting error")
+// console.log(2+2)
+// console.log("Geting error")
+
+let score = 33
+console.log(typeof(score));
+
+let score1 = "33"
+console.log(typeof(score1));
+
+// Changing the type of score1 from string to number
+
+let number = Number(score1)
+console.log(typeof(number));
+
+
+let score2 = "33rtc"
+console.log(typeof(score1));
+
+let numberconvert = Number(score2)
+console.log(typeof(numberconvert));
+console.log(numberconvert);
+
+
+// conversion
+// "33" => 33
+// "33abe" => NaN (not a number)
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let isLoggedIn = "disha"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+//"" => false
+// "disha" => true
